36afbfcb4e71e7616faec5f4cd78866955e8f0ad
ChynybekKazakbayev <chynybekkazakbaevuulu@gmail.com>
2019-09-29 22:35:37 +0600

    ispravil owibku

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,15 @@ import com.company.BankAccount;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         BankAccount bank = new BankAccount();
         bank.deposit(10000);
-        while (true){
-            bank.withDraw(1000);
+        while (bank.amount > 0){
+            try {
+                bank.withDraw(1000);
+            } catch (LimitException e) {
+                e.printStackTrace();
+            }
         }
 
     }
